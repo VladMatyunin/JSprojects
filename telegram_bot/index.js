5dbfdee75e46e52d8a8661c3e42ce4aeb123654c
Matyunin Vladislav <vlad.matyunin96@gmail.com>
2017-07-26 00:29:15 +0300

    init

---
+++ telegram_bot/index.js
@@ -0,0 +1,35 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+    // Устанавливаем токен, который выдавал нам бот.
+    var token = '';
+    // Включить опрос сервера
+    var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
+ 
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+});
+ 
+bot.on('text', function(msg)
+{
+    var messageChatId = msg.chat.id;
+    var messageText = msg.text;
+    var messageDate = msg.date;
+    var messageUsr = msg.from.username;
+ 
+    if (messageText === '/say') {
+        sendMessageByBot(messageChatId, 'Hello World!');
+    }
+ 
+    console.log(msg);
+});
+ 
+function sendMessageByBot(aChatId, aMessage)
+{
+    bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
+}
