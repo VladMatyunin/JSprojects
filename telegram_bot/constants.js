b44b67403d16f2f66b8f3c2c46eece7f71d0fbb1
Matyunin Vladislav <vlad.matyunin96@gmail.com>
2017-08-05 13:53:20 +0300

    constants added

---
+++ telegram_bot/constants.js
@@ -0,0 +1,39 @@
+module.exports = Object.freeze({
+
+ABOUT_KEY : 'about',
+HOW_TO_FIND_KEY : 'how to find us',
+SALES_KEY : 'sales',
+SUB_MENU_KEY : 'menu',
+
+
+MY_ACCOUNT_KEY : 'my account',
+WANT_DELIVERY_KEY : 'want delivery',
+QUESTIONS_KEY : 'questions',
+BACK_TO_MAIN_MENU_KEY : '< back',
+
+ACCOUNT_MY_BONUS_KEY : 'my bonus',
+ACCOUNT_QUESTION_KEY : 'question',
+ACCOUNT_BACK_TO_SUBMENU_KEY : '< back to menu',
+
+
+
+/*
+ *::::::::::::::::::::::::::::::::::::::::::::::::::::
+ *  ROUTES
+ *::::::::::::::::::::::::::::::::::::::::::::::::::::
+ */
+ABOUT_ROUTE : '/about',
+HOW_TO_FIND_ROUTE : '/find',
+SALES_ROUTE : '/sales',
+SUB_MENU_ROUTE : '/menu',
+
+
+MY_ACCOUNT_ROUTE : '/menu/account',
+WANT_DELIVERY_ROUTE : '/menu/delivery',
+QUESTIONS_ROUTE : '/menu/questions',
+BACK_TO_MAIN_MENU_ROUTE : '/menu/back',
+
+ACCOUNT_MY_BONUS_ROUTE : '/menu/account/bonus',
+ACCOUNT_QUESTION_ROUTE : '/menu/account/question',
+ACCOUNT_BACK_TO_SUBMENU_ROUTE : '/menu/account/back',
+});
